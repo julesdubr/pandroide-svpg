da062b7690cdc4d40b63cf67d8e66bb5af0465df
khiem2105.1 <khiem2105.1@gmail.com>
2022-04-05 00:25:37 +0000

    Add test

---
+++ test/test_reinforce.py
@@ -0,0 +1,15 @@
+import hydra
+
+from svpg.svpg_reinforce_mono import SVPG_Reinforce_Mono
+
+@hydra.main(config_path=".", config_name="test_reinforce.yaml")
+def main(cfg):
+    import torch.multiprocessing as mp
+    mp.set_start_method("spawn")
+
+    algo = SVPG_Reinforce_Mono(cfg)
+    algo.run_svpg()
+
+
+if __name__ == "__main__":
+    main()
