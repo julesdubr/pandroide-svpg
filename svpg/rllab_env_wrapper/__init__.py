a884f5144544e5d7ec03619362823ac87be603df
khiem2105.1 <khiem2105.1@gmail.com>
2022-05-01 09:06:15 +0000

    Fix xml model path

---
+++ svpg/rllab_env_wrapper/__init__.py
@@ -0,0 +1,19 @@
+import gym
+
+def register(id, entry_point, max_episode_steps=500):
+    env_specs = gym.envs.registry.env_specs
+
+    if id in env_specs.keys():
+        print("In")
+        del env_specs[id]
+    
+    gym.register(
+        id=id,
+        entry_point=entry_point,
+        max_episode_steps=max_episode_steps
+    ) 
+
+register(id="MyCartPole-v0", entry_point="svpg.rllab_env_wrapper.envs.rllab_cartpole:MyCartPole")
+register(id="MyCartPoleSwingUp-v0", entry_point="svpg.rllab_env_wrapper.envs.rllab_cartpoleswingup:MyCartPoleSwingUp")
+register(id="MyMountainCar-v0", entry_point="svpg.rllab_env_wrapper.envs.rllab_mountaincar:MyMountainCar")
+register(id="MyPendulum-v0", entry_point="svpg.rllab_env_wrapper.envs.rllab_pendulum:MyPendulum")
